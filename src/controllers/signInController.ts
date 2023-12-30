73fef0ecc0cac3ae05c2a8c72e6d07a3b01d00fd
Julio Cesar <juliomtd23@gmail.com>
2023-12-30 20:48:38 -0300

    feat: added sign in controller

--- src/controllers/signInController.ts
+++ src/controllers/signInController.ts
@@ -1,6 +1,54 @@
 import { Request, Response } from "express";
 import bcrypt from 'bcrypt';
+import UserSignIn from "../server/database/schemas/UserSignIn";
+import UserSignUp from "../server/database/schemas/UserSignUp";
 
-export const signInController = async(req:Request, res:Response) => {
+export const signInController = async (req: Request, res: Response) => {
+    try {
+        const user = await UserSignIn.findOne({ email: req.body.email });
 
-}
+        if (!user) {
+            return res.status(404).send({
+                "type": "Validation Error",
+                "errors": [
+                    {
+                        "resource": "email",
+                        "message": "Invalid email"
+                    }
+                ]
+            });
+        }
+
+        if (user.password && typeof user.password === 'string') {
+            const correctPassword = await bcrypt.compare(req.body.password, user.password);
+
+            if (correctPassword) {
+                // Busca o usuário na coleção UserSignUp para obter mais informações
+                const userSignUp = await UserSignUp.findOne({ email: req.body.email });
+
+                if (userSignUp) {
+                    return res.send({
+                        "firstName": userSignUp.firstName,
+                        "lastName": userSignUp.lastName,
+                        "email": userSignUp.email
+                    });
+                } else {
+                    return res.status(500).send('Erro interno do servidor');
+                }
+            } else {
+                return res.status(400).send({
+                    "type": "Validation Error",
+                    "errors": [
+                        {
+                            "resource": "password",
+                            "message": "Invalid password"
+                        }
+                    ]
+                });
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro interno do servidor');
+    }
+};
