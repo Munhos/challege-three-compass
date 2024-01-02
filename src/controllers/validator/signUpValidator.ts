5aa4872ed62fb204c367d662f87e3cbcaa43f047
Julio Cesar <juliomtd23@gmail.com>
2024-01-02 06:51:47 -0300

    fix: JOI bug

--- src/controllers/validator/signUpValidator.ts
+++ src/controllers/validator/signUpValidator.ts
@@ -2,11 +2,11 @@ import joi from "joi";
 
 export const signUpValidator = joi.object(
     {
-        firstName:joi.string().min(5).max(250).required(),
-        lastName:joi.string().min(5).max(250).required(),
+        firstName:joi.string().required(),
+        lastName:joi.string().required(),
         birthDate:joi.date().required(),
         city:joi.string(),
-        country:joi.string(),
+        coutry:joi.string(),
         email:joi.string(),
         password:joi.string().min(5).max(250).required(),
         confirmPassword:joi.string().min(5).max(250).required()
