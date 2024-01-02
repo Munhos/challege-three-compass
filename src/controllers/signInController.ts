0b5a009b1fdd7d78cf145ef2b412664e0c296a6a
Julio Cesar <juliomtd23@gmail.com>
2024-01-01 22:17:49 -0300

    refactor: Sign In controler

--- src/controllers/signInController.ts
+++ src/controllers/signInController.ts
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { type Request, type Response } from "express";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import UserSignIn from "../server/database/schemas/UserSignIn";
 import UserSignUp from "../server/database/schemas/UserSignUp";
@@ -6,7 +8,7 @@ import UserSignUp from "../server/database/schemas/UserSignUp";
 export const signInController = async (req: Request, res: Response) => {
   try {
     const user = await UserSignIn.findOne({ email: req.body.email });
-
+    
     if (user == null) {
       return res.status(404).send({
         type: "Validation Error",
@@ -19,37 +21,35 @@ export const signInController = async (req: Request, res: Response) => {
       });
     }
 
-    if (user.password != null && typeof user.password === "string") {
-      const correctPassword = await bcrypt.compare(req.body.password,
-        user.password,
-      );
+    
 
-      if (correctPassword) {
-        const userSignUp = await UserSignUp.findOne({ email: req.body.email });
-
-        if (userSignUp != null) {
-          return res.send({
-            firstName: userSignUp.firstName,
-            lastName: userSignUp.lastName,
-            email: userSignUp.email,
+    const correctPassword = await bcrypt.compare(req.body.password, user?.password ?? "");
+    if (correctPassword) {
+      const userSignUp = await UserSignUp.findOne({ email: req.body.email });
+      return res.status(200).send({
+        "firstName" : userSignUp?.firstName ?? "",
+        "lastName" : userSignUp?.lastName ?? "",
+        "email" : userSignUp?.email ?? ""
+      });
+      
+    } else {
+      return res.status(400).send({
+          "type": "Validation Error",
+          "errors": [
+              {
+                  "resource": "password",
+                  "message": "Invalid password"
+              }
+          ]
           });
-        } else {
-          return res.status(500).send("Erro interno do servidor");
-            }
-      } else {
-            return res.status(400).send({
-                "type": "Validation Error",
-                "errors": [
-                    {
-                        "resource": "password",
-                        "message": "Invalid password"
-                    }
-                ]
-                });
-            }
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Erro interno do servidor");
-    }
+      }
+
+  } catch {
+    return res.status(500).send({
+      "statusCode": 500,
+      "error": "Internal Server Error",
+      "message": "Something went wrong"
+    });
+  }
 };
+
